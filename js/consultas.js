4ece2ead236407679ef7378ca47efde5af05af6a
Rallge <geraldinelopz@gmail.com>
2021-05-31 03:37:39 +0200

    Ordenando funciones

--- js/consultas.js
+++ js/consultas.js
@@ -1,54 +1,20 @@
 const equiposMayoresEdad = (equipos, edad) =>
-    equipos.filter(
-        ({
-            asignado: {
-                empleado: { edad: edadEmpleado },
-            },
-        }) => edadEmpleado > edad
-    );
+  equipos.filter(
+    ({
+      asignado: {
+        empleado: { edad: edadEmpleado },
+      },
+    }) => edadEmpleado > edad
+  );
 const equiposProvincia = (equipos, provincia) =>
-    equipos.filter(
-        ({ asignado: { provincia: provinciaAsignado } }) =>
-        provinciaAsignado.toLowerCase() === provincia.toLowerCase()
-    );
-const equiposPorEdad = (equipos) =>
-    equipos.sort(
-        ({
-            asignado: {
-                empleado: { edad: edadTrabajadorA },
-            },
-        }, {
-            asignado: {
-                empleado: { edad: edadTrabajadorB },
-            },
-        }) => edadTrabajadorA - edadTrabajadorB
-    );
-const equiposTipo = (equipos, tipo) =>
-    equipos.filter(
-        ({ tipo: tipoEquipo }) => tipoEquipo.toLowerCase() === tipo.toLowerCase()
-    );
-const trabajadoresTipo = (equipos, tipo) =>
-    equipos
-    .filter(
-        ({ tipo: tipoEquipo }) => tipoEquipo.toLowerCase() === tipo.toLowerCase()
-    )
-    .map(({ asignado: { empleado } }) => empleado);
-const equiposTipoLocalidad = (equipos, tipo, localidad) =>
-    equipos.filter(
-        ({ tipo: equipoTipo, asignado: { provincia: trabajadoresProvincia } }) =>
-        equipoTipo.toLowerCase() === tipo.toLowerCase() &&
-        trabajadoresProvincia.toLowerCase() === localidad.toLowerCase()
-    );
-const resumenEquipos = (equipos) =>
-    equipos.map(({ id, asignado: { poblacion, provincia } }) => ({
-        id,
-        poblacion,
-        provincia,
-    }));
+  equipos.filter(
+    ({ asignado: { provincia: provinciaAsignado } }) =>
+      provinciaAsignado.toLowerCase() === provincia.toLowerCase()
+  );
 
 const provincias = (equipos) =>
-  equipos.map(({asignado:{provincia}})=>provincia)
-  .filter((provincia, index, provincias) => provincias.indexOf(provincia) === index);
+  equipos.map(({ asignado: { provincia } }) => provincia)
+    .filter((provincia, index, provincias) => provincias.indexOf(provincia) === index);
 
 const puestos = (equipos) =>
   equipos
@@ -60,8 +26,55 @@ const puestos = (equipos) =>
 const edadMedia = (equipos) =>
   equipos
     .map(({ asignado: { empleado: { edad } } }) =>
-    edad
-  ).reduce(
+      edad
+    ).reduce(
       (acumulador, edad, _index, edades) => edad / edades.length + acumulador,
       0
     );
+
+const equiposPorEdad = (equipos) =>
+  equipos.sort(
+    ({
+      asignado: {
+        empleado: { edad: edadTrabajadorA },
+      },
+    }, {
+      asignado: {
+        empleado: { edad: edadTrabajadorB },
+      },
+    }) => edadTrabajadorA - edadTrabajadorB
+  );
+const equiposTipo = (equipos, tipo) =>
+  equipos.filter(
+    ({ tipo: tipoEquipo }) => tipoEquipo.toLowerCase() === tipo.toLowerCase()
+  );
+const trabajadoresTipo = (equipos, tipo) =>
+  equipos
+    .filter(
+      ({ tipo: tipoEquipo }) => tipoEquipo.toLowerCase() === tipo.toLowerCase()
+    )
+    .map(({ asignado: { empleado } }) => empleado);
+const equiposTipoLocalidad = (equipos, tipo, localidad) =>
+  equipos.filter(
+    ({ tipo: equipoTipo, asignado: { provincia: trabajadoresProvincia } }) =>
+      equipoTipo.toLowerCase() === tipo.toLowerCase() &&
+      trabajadoresProvincia.toLowerCase() === localidad.toLowerCase()
+  );
+const resumenEquipos = (equipos) =>
+  equipos.map(({ id, asignado: { poblacion, provincia } }) => ({
+    id,
+    poblacion,
+    provincia,
+  }));
+
+const equiposPorTipo = (equipos) =>
+  equipos
+    .map
+    (({ tipo,id, asignado: { poblacion, provincia } }) => ({
+      tipo,
+      id,
+      poblacion,
+      provincia,
+
+    })).filter((puesto, index, puestos) => puestos.indexOf(puesto) === index);
+
